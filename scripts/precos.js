d7991ec212149f6a8bc947a4f18c74b665e98eda
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-08-01 00:09:33 -0300

    Thanks God

--- scripts/precos.js
+++ scripts/precos.js
@@ -2,22 +2,22 @@ function showInfo(weight) {
   let info = '';
   switch(weight) {
       case '500g':
-          info = '<span class="pink">Serve 5 a 8 pessoas</span><br>R$ 50 reais';
+          info = '<span class="pink">Serve 4 a 6 pessoas</span><br>R$ 50 reais';
           break;
       case '1kg':
-          info = '<span class="pink">Serve 10 a 15 pessoas</span><br>R$ 80 reais';
+          info = '<span class="pink">Serve 8 a 12 pessoas</span><br>R$ 80 reais';
           break;
       case '2kg':
-          info = '<span class="pink">Serve 20 a 30 pessoas</span><br>R$ 160 reais';
+          info = '<span class="pink">Serve 16 a 24 pessoas</span><br>R$ 160 reais';
           break;
       case '3kg':
-          info = '<span class="pink">Serve 30 a 40 pessoas</span><br>R$ 240 reais';
+          info = '<span class="pink">Serve 24 a 36 pessoas</span><br>R$ 240 reais';
           break;
       case '4kg':
-          info = '<span class="pink">Serve 40 a 60 pessoas</span><br>R$ 320 reais';
+          info = '<span class="pink">Serve 32 a 48 pessoas</span><br>R$ 320 reais';
           break;
       case '5kg':
-          info = '<span class="pink">Serve 70 a 90 pessoas</span><br>R$ 400 reais';
+          info = '<span class="pink">Serve 40 a 60 pessoas</span><br>R$ 400 reais';
           break;
       default:
           info = '';
