e5dc008a520fbf9aacd17e9e14e26164b7435429
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-08-01 16:19:54 -0300

    Thanks God

--- scripts/cupom.js
+++ scripts/cupom.js
@@ -10,7 +10,7 @@
       // Função para redirecionar o usuário ao clicar no botão
       function redirect() {
           const phoneNumber = "5516997677955";  // Número do WhatsApp com código do país e DDD
-          const message = `Oi Sandra, gostaria de fazer um pedido usando meu cupom de desconto de 10% [ *${couponNumber}* ]`;
+          const message = `Olá, vim pelo site gostaria de fazer o pedido com os descontos do site !! Esse é o meu cupom *🡺${couponNumber}🡸*`;
           const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
           window.location.href = url;
       }
